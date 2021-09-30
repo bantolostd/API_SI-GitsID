a7c22813ac66d9ce32dde35d3451f5163f03de0b
bantolostd <bantolostd@gmail.com>
2021-09-30 07:02:41 +0700

    update database

--- pengguna.sql
+++ pengguna.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Sep 2021 pada 14.31
+-- Waktu pembuatan: 30 Sep 2021 pada 02.01
 -- Versi server: 10.4.19-MariaDB
 -- Versi PHP: 8.0.7
 
@@ -33,7 +33,7 @@ CREATE TABLE `pengguna` (
   `pengguna_email` varchar(20) NOT NULL,
   `pengguna_username` varchar(20) NOT NULL,
   `pengguna_password` varchar(20) NOT NULL,
-  `pengguna_foto` varchar(20) NOT NULL
+  `pengguna_foto` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -41,7 +41,8 @@ CREATE TABLE `pengguna` (
 --
 
 INSERT INTO `pengguna` (`pengguna_id`, `pengguna_nama`, `pengguna_email`, `pengguna_username`, `pengguna_password`, `pengguna_foto`) VALUES
-(1, 'Bantolo Setiadi', 'bantolostd@gmail.com', 'bantolostd', 'admin', '');
+(1, 'Bantolo Setiadi', 'bantolostd@gmail.com', 'bantolostd', 'admin', 'user_default.jpg'),
+(2, 'Setiadi Bantolo', 'stdbantolo@gmail.com', 'stdbantolo', 'minad', 'The autumn explorer12491_rectangle.jpg');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +62,7 @@ ALTER TABLE `pengguna`
 -- AUTO_INCREMENT untuk tabel `pengguna`
 --
 ALTER TABLE `pengguna`
-  MODIFY `pengguna_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `pengguna_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
